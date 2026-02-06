a378e6167b34a5429ab4da1a46bef365a55c19b8
Hafijur <hafiz55vu@gmail.com>
2026-02-06 23:34:38 +0600

    I add some query(9-18)

--- EXP3.sql
+++ EXP3.sql
@@ -19,31 +19,10 @@ INSERT INTO WORKER VALUES
 
 SELECT * FROM WORKER;
 
-SELECT COUNT(SALARY) FROM WORKER;
-
-SELECT COUNT(*) FROM WORKER;
-
-SELECT MAX(SALARY) FROM WORKER;
-
-SELECT 
-COUNT(*) AS Total_Worker,
-SUM(SALARY) AS Total_Salary,
-AVG(SALARY) AS Average_Salary,
-MAX(SALARY) AS Highest_Salary,
-MIN(SALARY) AS Lowest_Salary
-FROM WORKER;
-
-SELECT DEPARTMENT,SUM(SALARY) AS TOTAL_SALARY
-FROM WORKER 
-GROUP BY DEPARTMENT;
-
-SELECT DEPARTMENT , SUM(SALARY) AS TOTAL_SALARY
-FROM WORKER
-GROUP BY DEPARTMENT
-HAVING  SUM(SALARY) > 350000;
+DROP TABLE WORKER;
 
 --1. Write an SQL query to print first three characters of  FIRST_NAME from Worker table. 
-SELECT LEFT(FIRST_NAME,3) FROM WORKER;
+SELECT LEFT(FIRST_NAME,3) AS WORKER_NAME FROM WORKER;
 
 SELECT SUBSTRING(FIRST_NAME,1,3) FROM WORKER;
 
@@ -56,8 +35,9 @@ WHERE DATEDIFF(MONTH,JOINING_DATE,GETDATE()) >= 6;
 --4. Write an SQL query to update all worker salary whose title is manager. 
 UPDATE WORKER SET SALARY = SALARY + 5000
 WHERE 
---5. Write an SQL query to update all worker bonus 10% whose joining_date before ï¿½2014
---04-11 09:00:00ï¿½ otherwise bonus update 5% and also check department name is ï¿½Adminï¿½.
+
+--5. Write an SQL query to update all worker bonus 10% whose joining_date before ‘2014
+--04-11 09:00:00’ otherwise bonus update 5% and also check department name is ‘Admin’.
 UPDATE WORKER
 SET BONUS = CASE 
                 WHEN JOINING_DATE < '2014-04-11 09:00:00' THEN SALARY * 0.10
@@ -65,18 +45,66 @@ SET BONUS = CASE
             END
 WHERE DEPARTMENT = 'Admin';
 
---6. Write an SQL query to delete all workers who have not taken any bonus. 
+--6. Write an SQL query to delete all workers who have not taken any bonus.
 
---7. Write an SQL query to print details for Workers with the first name ï¿½Ranaï¿½ and ï¿½Sajibï¿½ 
+
+--7. Write an SQL query to print details for Workers with the first name “Rana” and “Sajib” 
 --from Worker table. 
+SELECT * FROM WORKER WHERE FIRST_NAME IN ('Rana','Sajib');
 
---8. Write an SQL query to print details of workers excluding first names, ï¿½Ranaï¿½ and ï¿½Sajibï¿½ 
---from Worker table. 
+--8. Write an SQL query to print details of workers excluding first names, “Rana” and “Sajib” 
+--from Worker table.
+SELECT * FROM WORKER WHERE FIRST_NAME NOT IN ('Rana','Sajib');
 
---9. Write an SQL query to print details of the Workers whose FIRST_NAME contains ï¿½aï¿½. 
+--9. Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’.
 
---10. Write an SQL query to print details of the Workers whose FIRST_NAME starts with ï¿½kï¿½.
+SELECT * FROM WORKER WHERE FIRST_NAME LIKE '%a%';
 
---11. Write an SQL query to print details of the Workers whose FIRST_NAME ends with ï¿½rï¿½ 
---and contains seven alphabets. 
+--10. Write an SQL query to print details of the Workers whose FIRST_NAME starts with ‘k’.
 
+SELECT * FROM WORKER WHERE FIRST_NAME LIKE 'k%';
+
+--11. Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘r’ 
+--and contains seven alphabets.
+SELECT * FROM WORKER WHERE FIRST_NAME LIKE '%r';
+--12. Write an SQL query to find the position of the alphabet (‘n’) in the FIRST_NAME 
+--column ‘Sanjoy’ from Worker table.
+SELECT CHARINDEX('n',FIRST_NAME) AS POSITION_OF_N
+FROM WORKER
+WHERE FIRST_NAME = 'Sanjoy';
+
+--13.  Find the average salary of employees for each department.
+SELECT DEPARTMENT, AVG(SALARY) AS AVG_SALARY
+FROM WORKER
+GROUP BY DEPARTMENT;
+
+--14. List all the employees who have maximum or minimum salary in each department
+SELECT * FROM WORKER W
+WHERE SALARY = (      
+        SELECT MAX(SALARY)
+        FROM WORKER 
+        WHERE DEPARTMENT = W.DEPARTMENT
+        )
+     OR SALARY = (
+        SELECT MIN(SALARY)
+        FROM WORKER 
+        WHERE DEPARTMENT = W.DEPARTMENT
+        );
+
+--15. Write an SQL query to find the position of the alphabet (‘r’) in the FIRST_NAME
+--column ‘Rana’ from Worker table
+
+SELECT CHARINDEX('r',FIRST_NAME) AS POSITION_OF_R FROM WORKER WHERE FIRST_NAME ='Rana';
+
+--16. Write an SQL query to print the FIRST_NAME from Worker table after removing white
+--spaces from the right side.
+SELECT RTRIM(FIRST_NAME) AS FIRST_NAME FROM WORKER;
+
+--17. Write an SQL query that fetches the unique values of FIRST_NAME from Worker table
+--and prints its length.
+
+SELECT DISTINCT FIRST_NAME,LEN(FIRST_NAME) AS NAME_LENGTH FROM WORKER;
+
+--18. Write an SQL query to print the FIRST_NAME from Worker table after replacing ‘a’
+--with ‘A’.
+SELECT REPLACE(FIRST_NAME,'a','A') FROM WORKER;
